6ecbdf09f0ad5b4f1f8d4c398ba8c1fc9862fa4d
Willian Silva <silva.williansantos@gmail.com>
2020-09-24 23:27:50 +0000

    Create initials components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StylesProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import GlobalStyle from 'commons/styles/GlobalStyles';
+import Main from 'containers/Main';
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
+      <Main />
     </StylesProvider>
   );
 }
