48577388a8d7e1e3eba85c03864fce6ae2f7aa01
Willian Silva <silva.williansantos@gmail.com>
2020-09-26 00:49:18 +0000

    PWA Init

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -68,9 +68,9 @@ function Panel({ updateAt, onChange, data, country, getCovidData }) {
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
-          <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
+          <Typography variant="h5" component="div" color="primary">COVID19</Typography>
+          <Typography variant="h6" component="div" color="primary">Painel Coronavírus</Typography>
+          <Typography variant="body2" component="div" color="primary">Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
               {COUNTRIES.map(renderCountries)}
